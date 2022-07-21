b9d5e8cc73f14bce9e2ab453e70b467f0dfff709
Monroy2021 <luismonroy1014@gmail.com>
2022-07-20 22:57:19 -0500

    Creacion del proyecto, creacion del logim con su autenticacion con firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyApiw1JpbYvUHLdXtKhf7nTpUVKuxRLLbo",
+    authDomain: "superheroes-69be7.firebaseapp.com",
+    projectId: "superheroes-69be7",
+    storageBucket: "superheroes-69be7.appspot.com",
+    messagingSenderId: "1025915244786",
+    appId: "1:1025915244786:web:cb035c8d18a9b95fe1c06b"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
