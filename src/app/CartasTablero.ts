1a352b4f48343aa1a2044140657df9d7b3e528b7
juan9507 <juandavidnaranjo75@gmail.com>
2022-07-25 20:31:56 -0500

    creacion del tablero datos quemados

---
+++ src/app/CartasTablero.ts
@@ -0,0 +1,4 @@
+export interface CartasTablero {
+  id: string;
+  url: string;
+}
--- src/app/components/tablero/tablero.component.ts
+++ src/app/components/tablero/tablero.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { CartasTablero } from '../../CartasTablero';
 
 @Component({
   selector: 'app-tablero',
   templateUrl: './tablero.component.html',
-  styleUrls: ['./tablero.component.css']
+  styleUrls: ['./tablero.component.css'],
 })
 export class TableroComponent implements OnInit {
+  CARTATABLERO: CartasTablero[] = [
+    { id: 'fhfghfghytuj', url: '../../../assets/img/heroes/0BF.jpg' },
+    { id: 'fhfghfghytuj', url: '../../../assets/img/heroes/0E0.jpg' },
+    { id: 'fhfghfghytuj', url: '../../../assets/img/heroes/1AE.jpg' },
+    { id: 'fhfghfghytuj', url: '../../../assets/img/heroes/1DF.jpg' },
+    { id: 'fhfghfghytuj', url: '../../../assets/img/heroes/1F4.jpg' },
+  ];
+  // cartaTablero: CartasTablero = {
+  //   id: 'fhfghfghytuj',
+  //   url: '../../../assets/img/heroes/0BF.jpg',
+  // };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
